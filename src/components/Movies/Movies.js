c499f259f8c14f44267c2cb93585fec3dc14b321
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-11-07 10:01:05 +0200

    Update Movies.js

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Wrap, InputSearch } from './Movies.Syled';
 
 export const Movies = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-
   return (
     <Wrap>
       <InputSearch type="text" onChange={e => console.log(e.target.value)} />
